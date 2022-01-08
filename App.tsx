f64f0280d0b9b24a08fcb131974a6b9fee639c75
minbyoungdae <80196999+minbyoungdae@users.noreply.github.com>
2022-01-09 02:45:35 +0900

    Fix: 타이틀 추가

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import InputRoutine from "./components/InputRoutine";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.titleContainer}>
+        <FontAwesome5
+          name="dumbbell"
+          size={24}
+          color="black"
+          style={styles.title}
+        />
+        <Text style={styles.title}>Routiney</Text>
+      </View>
+      <View style={styles.mainContainer}>
+        <InputRoutine />
+      </View>
+      <StatusBar></StatusBar>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  titleContainer: {
+    flex: 1,
+    fontSize: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    textAlign: "center",
+    fontWeight: "700",
+    fontSize: 30,
+    marginVertical: 10,
+  },
+  mainContainer: {
+    flex: 2,
+    alignItems: "center",
   },
 });
+
+export default App;
