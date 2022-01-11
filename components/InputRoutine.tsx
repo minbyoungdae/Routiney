f41cc25d99356b9bb3709c64a0a78b0c074d9057
minbyoungdae <80196999+minbyoungdae@users.noreply.github.com>
2022-01-11 23:05:55 +0900

    Fix: 시작 종료 버튼 추가

--- components/InputRoutine.tsx
+++ components/InputRoutine.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useCallback } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import {
   StyleSheet,
   Pressable,
@@ -6,31 +6,25 @@ import {
   View,
   TextInput,
   Alert,
-  Button,
 } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import ExerciseList from "./ExerciseList";
 import Tts from "./Tts";
 
 const InputRoutine = () => {
-  const [list, setList] = useState<any | []>([
-    {
-      minute: "3",
-      exercise: "팔굽혀펴기",
-    },
-    {
-      minute: "5",
-      exercise: "윗몸일으키기",
-    },
-    {
-      minute: "3",
-      exercise: "달리기",
-    },
-  ]);
-  const [minute, setMinute] = useState<any>("");
-  const [exercise, setExercise] = useState<any>("");
-  const [breakTime, setBreakTime] = useState<number>();
+  const [list, setList] = useState<any>([]);
+  const [minute, setMinute] = useState<string>();
+  const [exercise, setExercise] = useState<string>();
+  const [breakTime, setBreakTime] = useState<string>();
+  const [activeButton, setActiveButton] = useState<Boolean>(false);
   const onPress = () => {
-    setList([...list, { minute: minute, exercise: exercise }]);
+    if (!(minute && exercise)) {
+      Alert.alert("시간과 운동 이름을 입력해주세요");
+    } else if (!list.length) {
+      setList([{ minute: minute, exercise: exercise }]);
+    } else {
+      setList([...list, { minute: minute, exercise: exercise }]);
+    }
     setMinute("");
     setExercise("");
     inputRef.current.blur();
@@ -40,19 +34,88 @@ const InputRoutine = () => {
   const deleteList = (index: number) => {
     setList(list.filter((e: any, idx: number) => idx !== index));
   };
+
+  const saveListData = async () => {
+    try {
+      await AsyncStorage.setItem("@list", JSON.stringify(list));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const saveBTData = async () => {
+    try {
+      await AsyncStorage.setItem("@BT", JSON.stringify(breakTime));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const listData: any = await AsyncStorage.getItem("@list");
+      const JSONListData: any = JSON.parse(listData);
+      if (JSONListData !== null) {
+        setList(JSONListData);
+      } else {
+        setList([]);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getBTData = async () => {
+    const breakTimeData: any = await AsyncStorage.getItem("@BT");
+    const JSONBTData: any = JSON.parse(breakTimeData);
+    if (JSONBTData !== null) {
+      setBreakTime(JSONBTData);
+    } else {
+      setBreakTime("");
+    }
+  };
+
+  useEffect(() => {
+    getData();
+    getBTData();
+  }, []);
+
+  useEffect(() => {
+    saveListData();
+  }, [list]);
+
+  useEffect(() => {
+    saveBTData();
+  }, [breakTime]);
+
+  // todo 시작 중이면 시작 버튼 색 바뀌고, 종료되면 다시 바뀌기
   return (
     <>
-      <View>
-        <Pressable onPress={() => Tts(list, breakTime)}>
+      <View
+        style={{
+          ...styles.container,
+          justifyContent: "space-around",
+          marginBottom: 30,
+        }}
+      >
+        <Pressable
+          style={activeButton ? styles.activeButton : styles.startButton}
+          onPress={() => Tts("start", setActiveButton, list, breakTime)}
+        >
           <Text>시작</Text>
+        </Pressable>
+        <Pressable onPress={() => Tts("stop", setActiveButton)}>
+          <Text>종료</Text>
         </Pressable>
       </View>
       <View style={styles.container}>
         <TextInput
           style={styles.breakTimeInput}
-          placeholder="쉬는 시간"
+          placeholder="쉬는 시간(분)"
           ref={inputRef}
-          onChangeText={(text) => setBreakTime(Number(text))}
+          value={String(breakTime) ?? ""}
+          onChangeText={(text) => setBreakTime(text)}
+          keyboardType="number-pad"
         ></TextInput>
       </View>
       <View
@@ -127,6 +190,36 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     letterSpacing: 0.25,
     color: "white",
+  },
+  startButton: {
+    backgroundColor: "#eee",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 70,
+    height: 50,
+    borderRadius: 25,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 1,
+      height: 4,
+    },
+    shadowOpacity: 1,
+  },
+  activeButton: {
+    backgroundColor: "#eee",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 70,
+    height: 50,
+    borderRadius: 25,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowOpacity: 1,
+    position: "relative",
+    top: 2,
   },
 });
 
