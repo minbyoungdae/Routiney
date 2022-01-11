b4b7bfdb1a350f5624f86cb47090c0643339a287
minbyoungdae <80196999+minbyoungdae@users.noreply.github.com>
2022-01-11 23:08:51 +0900

    Fix: 바깥 영역 클릭 시 키보드 다운 구현

--- App.tsx
+++ App.tsx
@@ -1,25 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Keyboard,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
 import InputRoutine from "./components/InputRoutine";
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.titleContainer}>
-        <FontAwesome5
-          name="dumbbell"
-          size={24}
-          color="black"
-          style={styles.title}
-        />
-        <Text style={styles.title}>Routiney</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <View style={styles.titleContainer}>
+          <FontAwesome5
+            name="dumbbell"
+            size={24}
+            color="black"
+            style={styles.title}
+          />
+          <Text style={styles.title}>Routiney</Text>
+        </View>
+        <View style={styles.mainContainer}>
+          <InputRoutine />
+        </View>
+        <StatusBar></StatusBar>
       </View>
-      <View style={styles.mainContainer}>
-        <InputRoutine />
-      </View>
-      <StatusBar></StatusBar>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
