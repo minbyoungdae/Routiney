a92387486f97db555fc3f81a58d209c4184ffe11
minbyoungdae <80196999+minbyoungdae@users.noreply.github.com>
2022-01-14 18:19:48 +0900

    Fix: 함수 모음으로 변경

--- components/Tts.tsx
+++ components/Tts.tsx
@@ -1,52 +1,64 @@
-import { StyleSheet, Pressable, Text, View } from "react-native";
 import * as Speech from "expo-speech";
-import { useCallback, useState } from "react";
 
-const Tts = async (
-  type: string,
-  setActiveButton?: any,
-  list?: any,
-  breakTime?: string
-) => {
-  let sleep = (ms: any) =>
-    new Promise((resolve) => setTimeout(resolve, Number(ms)));
-  const speakWord = (word: string) => {
-    Speech.speak(word, { rate: 0.8 });
-  };
-  const [refreshing, setRefreshing] = useState(false);
-  const onRefresh = () => {
-    setRefreshing(!refreshing);
-  };
+let sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-  const doSomething = async () => {
-    speakWord("루틴 시작");
-    for (const [index, e] of list.entries()) {
-      console.log(type);
-      speakWord(`${e.minute}분`);
-      speakWord(e.exercise);
-      await sleep(e.minute * 10000);
-      if (breakTime) {
-        if (index !== list.length - 1) {
-          speakWord("쉬는 시간");
-          await sleep(Number(breakTime) * 10000);
-        }
-      }
-      if (index === list.length - 1) {
-        // speakWord("루틴 종료");
-        setActiveButton(false);
-      }
-    }
-  };
-
-  if (type === "start") {
-    setActiveButton(true);
-    doSomething();
-    speakWord("루틴 종료");
-  } else if (type === "stop") {
-    setActiveButton(false);
-    Speech.stop();
-    // onRefresh();
-  }
+const speakWord = (word: string) => {
+  Speech.speak(word, { rate: 0.8 });
 };
 
-export default Tts;
+let timer: ReturnType<typeof setInterval>;
+let aaa = true;
+
+export const startTTS = (
+  list: Array<Object>,
+  breakTime: string,
+  setPlay: Function
+) => {
+  aaa = true;
+  timer = setInterval(() => {
+    rotineSpeak(0, list, breakTime, setPlay);
+    clearInterval(timer);
+  });
+};
+
+export const stopTTS = () => {
+  clearInterval(timer);
+  Speech.stop();
+  aaa = false;
+};
+
+interface IObject {
+  minute?: string;
+  exercise?: string;
+}
+
+export const rotineSpeak = async (
+  idx: number,
+  list: Array<IObject>,
+  breakTime: string,
+  setPlay: Function
+) => {
+  if (idx === 0) {
+    speakWord("루틴 시작");
+  }
+  if (idx === list.length) {
+    return;
+  }
+  speakWord(`${list[idx].minute} 분`);
+  speakWord(list[idx].exercise);
+  await sleep(Number(list[idx].minute) * 60000).then(async () => {
+    if (idx === list.length - 1) {
+      speakWord("루틴 종료");
+      setPlay(false);
+      return;
+    }
+    if (aaa) {
+      speakWord("쉬는 시간");
+      await sleep(Number(breakTime) * 60000).then(() => {
+        if (aaa) {
+          return rotineSpeak(idx + 1, list, breakTime, setPlay);
+        }
+      });
+    }
+  });
+};
